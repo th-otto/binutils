8ee1b5a59c32a718063d95bd4995c7a2356ab775
H.J. Lu <hjl.tools@gmail.com>
2023-08-21 11:26:44 +0200

    This reverts the below commit to not generate PLT32 relocs on branches by default. Used for old distros to not have to update several packages/tools that can't handle them. I.e. a compatibility patch.
        The patch isn't exactly the reverse of commit bd7ab16b
        because commit 83924b38 later moved the checking code
        around somewhat.
    
        The changes in nop-[345].d and pr22842b.S are followups to
        not break the testsuite because of this revert.
        As are the changes to x86-64-branch-2.d and x86-64-branch-3.d.
    
        x86-64: Generate branch with PLT32 relocation
    
        Since there is no need to prepare for PLT branch on x86-64, generate
        R_X86_64_PLT32, instead of R_X86_64_PC32, if possible, which can be
        used as a marker for 32-bit PC-relative branches.
    
        To compile Linux kernel, this patch:
    
        From: "H.J. Lu" <hjl.tools@gmail.com>
        Subject: [PATCH] x86: Treat R_X86_64_PLT32 as R_X86_64_PC32
    
        On i386, there are 2 types of PLTs, PIC and non-PIC.  PIE and shared
        objects must use PIC PLT.  To use PIC PLT, you need to load
        _GLOBAL_OFFSET_TABLE_ into EBX first.  There is no need for that on
        x86-64 since x86-64 uses PC-relative PLT.
    
        On x86-64, for 32-bit PC-relative branches, we can generate PLT32
        relocation, instead of PC32 relocation, which can also be used as
        a marker for 32-bit PC-relative branches.  Linker can always reduce
        PLT32 relocation to PC32 if function is defined locally.   Local
        functions should use PC32 relocation.  As far as Linux kernel is
        concerned, R_X86_64_PLT32 can be treated the same as R_X86_64_PC32
        since Linux kernel doesn't use PLT.
    
        is needed.  It is available on hjl/plt32/master branch at
    
        https://github.com/hjl-tools/linux
    
        bfd/
    
        	PR gas/22791
        	* elf64-x86-64.c (is_32bit_relative_branch): Removed.
        	(elf_x86_64_relocate_section): Check PIC relocations in PIE.
        	Remove is_32bit_relative_branch usage.  Disallow PC32 reloc
        	against protected function in shared object.
    
        gas/
    
        	PR gas/22791
        	* config/tc-i386.c (need_plt32_p): New function.
        	(output_jump): Generate BFD_RELOC_X86_64_PLT32 if possible.
        	(md_estimate_size_before_relax): Likewise.
        	* testsuite/gas/i386/reloc64.d: Updated.
        	* testsuite/gas/i386/x86-64-jump.d: Likewise.
        	* testsuite/gas/i386/x86-64-mpx-branch-1.d: Likewise.
        	* testsuite/gas/i386/x86-64-mpx-branch-2.d: Likewise.
        	* testsuite/gas/i386/x86-64-relax-2.d: Likewise.
        	* testsuite/gas/i386/x86-64-relax-3.d: Likewise.
        	* testsuite/gas/i386/ilp32/reloc64.d: Likewise.
        	* testsuite/gas/i386/ilp32/x86-64-branch.d: Likewise.
    
        ld/
    
        	PR gas/22791
        	* testsuite/ld-x86-64/mpx1c.rd: Updated.
        	* testsuite/ld-x86-64/pr22791-1.err: New file.
        	* testsuite/ld-x86-64/pr22791-1a.c: Likewise.
        	* testsuite/ld-x86-64/pr22791-1b.s: Likewise.
        	* testsuite/ld-x86-64/pr22791-2.rd: Likewise.
        	* testsuite/ld-x86-64/pr22791-2a.s: Likewise.
        	* testsuite/ld-x86-64/pr22791-2b.c: Likewise.
        	* testsuite/ld-x86-64/pr22791-2c.s: Likewise.
        	* testsuite/ld-x86-64/x86-64.exp: Run PR ld/22791 tests.

--- gas/testsuite/gas/i386/x86-64-jump.d
+++ gas/testsuite/gas/i386/x86-64-jump.d
@@ -9,7 +9,7 @@ Disassembly of section .text:
 
 0+ <.text>:
 [ 	]*[a-f0-9]+:	eb fe                	jmp    (0x0|0 <.text>)
-[ 	]*[a-f0-9]+:	e9 00 00 00 00       	jmp    0x7	3: R_X86_64_PLT32	xxx-0x4
+[ 	]*[a-f0-9]+:	e9 00 00 00 00       	jmp    0x7	3: R_X86_64_PC32	xxx-0x4
 [ 	]*[a-f0-9]+:	ff 24 25 00 00 00 00 	jmp    \*0x0	a: R_X86_64_32S	xxx
 [ 	]*[a-f0-9]+:	ff e7                	jmp    \*%rdi
 [ 	]*[a-f0-9]+:	ff 27                	jmp    \*\(%rdi\)
@@ -18,7 +18,7 @@ Disassembly of section .text:
 [ 	]*[a-f0-9]+:	ff 2c 25 00 00 00 00 	ljmp   \*0x0	24: R_X86_64_32S	xxx
 [ 	]*[a-f0-9]+:	66 ff 2c 25 00 00 00 00 	ljmpw  \*0x0	2c: R_X86_64_32S	xxx
 [ 	]*[a-f0-9]+:	e8 cb ff ff ff       	call   0x0
-[ 	]*[a-f0-9]+:	e8 00 00 00 00       	call   0x3a	36: R_X86_64_PLT32	xxx-0x4
+[ 	]*[a-f0-9]+:	e8 00 00 00 00       	call   0x3a	36: R_X86_64_PC32	xxx-0x4
 [ 	]*[a-f0-9]+:	ff 14 25 00 00 00 00 	call   \*0x0	3d: R_X86_64_32S	xxx
 [ 	]*[a-f0-9]+:	ff d7                	call   \*%rdi
 [ 	]*[a-f0-9]+:	ff 17                	call   \*\(%rdi\)
--- gas/testsuite/gas/i386/x86-64-relax-2.d
+++ gas/testsuite/gas/i386/x86-64-relax-2.d
@@ -11,12 +11,12 @@ Disassembly of section .text:
 0+ <foo>:
 [ 	]*[a-f0-9]+:	eb 24                	jmp    26 <local>
 [ 	]*[a-f0-9]+:	eb 1e                	jmp    22 <hidden_def>
-[ 	]*[a-f0-9]+:	e9 00 00 00 00       	jmp    9 <foo\+0x9>	5: R_X86_64_PLT32	global_def-0x4
+[ 	]*[a-f0-9]+:	e9 00 00 00 00       	jmp    9 <foo\+0x9>	5: R_X86_64_PC32	global_def-0x4
 [ 	]*[a-f0-9]+:	e9 00 00 00 00       	jmp    e <foo\+0xe>	a: R_X86_64_PLT32	global_def-0x4
-[ 	]*[a-f0-9]+:	e9 00 00 00 00       	jmp    13 <foo\+0x13>	f: R_X86_64_PLT32	weak_def-0x4
-[ 	]*[a-f0-9]+:	e9 00 00 00 00       	jmp    18 <foo\+0x18>	14: R_X86_64_PLT32	weak_hidden_undef-0x4
-[ 	]*[a-f0-9]+:	e9 00 00 00 00       	jmp    1d <foo\+0x1d>	19: R_X86_64_PLT32	weak_hidden_def-0x4
-[ 	]*[a-f0-9]+:	e9 00 00 00 00       	jmp    22 <hidden_def>	1e: R_X86_64_PLT32	hidden_undef-0x4
+[ 	]*[a-f0-9]+:	e9 00 00 00 00       	jmp    13 <foo\+0x13>	f: R_X86_64_PC32	weak_def-0x4
+[ 	]*[a-f0-9]+:	e9 00 00 00 00       	jmp    18 <foo\+0x18>	14: R_X86_64_PC32	weak_hidden_undef-0x4
+[ 	]*[a-f0-9]+:	e9 00 00 00 00       	jmp    1d <foo\+0x1d>	19: R_X86_64_PC32	weak_hidden_def-0x4
+[ 	]*[a-f0-9]+:	e9 00 00 00 00       	jmp    22 <hidden_def>	1e: R_X86_64_PC32	hidden_undef-0x4
 
 0+22 <hidden_def>:
 [ 	]*[a-f0-9]+:	c3                   	ret
